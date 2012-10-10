af90c1a7c0c9bbbcaedff285798e3cd1007d9f20
Emilio López <turl@tuxfamily.org>
2012-10-10 21:59:42 +0000

    Update pds clone script
    We're using Motorola UIDs now, so we don't need to fix them

--- scripts/pds_perm_fix.sh
+++ scripts/pds_perm_fix.sh
@@ -1,34 +1,16 @@
 #!/system/bin/sh
 export PATH=/system/xbin:$PATH
 
-if [ ! -f /data/pds-CM7.img ]
+if [ ! -f /data/pds-CM10.img ]
 then
     #make a copy of pds in /data
-    dd if=/dev/block/mmcblk0p3 of=/data/pds-CM7.img
+    dd if=/dev/block/mmcblk0p3 of=/data/pds-CM10.img
 
-    #mount the fake pds
-    /system/xbin/losetup /dev/block/loop0 /data/pds-CM7.img
-    /system/xbin/busybox mount -o rw /dev/block/loop0 /pds
-
-    cd /pds
-    #find and fix moto users first
-    /system/xbin/busybox find -user 9000 -exec chown 1000 {} \;
-    /system/xbin/busybox find -user 9003 -exec chown 1000 {} \;
-    /system/xbin/busybox find -user 9004 -exec chown 1000 {} \;
-    /system/xbin/busybox find -user 9007 -exec chown 1000 {} \;
-
-    #find and fix moto grps
-    /system/xbin/busybox find -group 9000 -exec chgrp 1000 {} \;
-    /system/xbin/busybox find -group 9003 -exec chgrp 1000 {} \;
-    /system/xbin/busybox find -group 9004 -exec chgrp 1000 {} \;
-    /system/xbin/busybox find -group 9007 -exec chgrp 1000 {} \;
-    /system/xbin/busybox find -group 9009 -exec chgrp 1000 {} \;
-
-    echo "Backed up, permission fixed and mounted PDS"
-else
-    #mount the fake pds
-    /system/xbin/losetup /dev/block/loop0 /data/pds-CM7.img
-    /system/xbin/busybox mount -o rw /dev/block/loop0 /pds
-
-    echo "Mounted PDS only."
+    echo "Backed up PDS"
 fi
+
+#mount the fake pds
+/system/xbin/losetup /dev/block/loop0 /data/pds-CM10.img
+/system/xbin/busybox mount -o rw /dev/block/loop0 /pds
+
+echo "Mounted PDS"

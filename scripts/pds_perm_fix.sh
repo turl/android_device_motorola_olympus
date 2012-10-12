41469c8e4411cb4ea211ecc56d442a23d39f8440
Emilio López <turl@tuxfamily.org>
2012-10-12 02:24:26 +0000

    Move PDS backup to /cache

--- scripts/pds_perm_fix.sh
+++ scripts/pds_perm_fix.sh
@@ -1,16 +1,16 @@
 #!/system/bin/sh
 export PATH=/system/xbin:$PATH
 
-if [ ! -f /data/pds-CM10.img ]
+if [ ! -f /cache/pds-CM10.img ]
 then
     #make a copy of pds in /data
-    dd if=/dev/block/mmcblk0p3 of=/data/pds-CM10.img
+    dd if=/dev/block/mmcblk0p3 of=/cache/pds-CM10.img
 
     echo "Backed up PDS"
 fi
 
 #mount the fake pds
-/system/xbin/losetup /dev/block/loop0 /data/pds-CM10.img
+/system/xbin/losetup /dev/block/loop0 /cache/pds-CM10.img
 /system/xbin/busybox mount -o rw /dev/block/loop0 /pds
 
 echo "Mounted PDS"
